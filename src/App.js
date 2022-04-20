7b25256f3efe3ddd396e867ac72b561ecf8c9237
Recep Terzi <recepterzi-67@hotmail.com>
2022-04-20 21:43:11 +0300

    React_Movies_App

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }, []);
+
+  
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+          console.log(data.results);
+        });
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  return (
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search"
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <Movie {...movie} key={movie.id} />
+        ))} 
+      </div>
+    </>
+  );
+}
+
+export default App;
